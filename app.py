7dbd64a1e95aa60af4288efb2cac4f7d34459d1b
najiya <najiyakk11@gmail.com>
2026-02-22 12:33:34 +0530

    update app for production

--- app.py
+++ app.py
@@ -69,6 +69,7 @@ def predict():
         phone=session.get('phone')
     )
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
